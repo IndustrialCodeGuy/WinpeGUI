d2445ed2e471654aa17baf5472a688d5e51cf6f6
IndustrialCodeGuy <Dmichel@farmward.net>
2026-08-09 02:02:27 -0500

    Add files via upload

---
+++ Explorer.Host/Startup/SingleInstanceGate.cs
@@ -0,0 +1,27 @@
+﻿namespace Explorer.Host.Startup;
+
+internal sealed class SingleInstanceGate : IDisposable
+{
+    private readonly Mutex _mutex;
+    public bool IsPrimaryInstance { get; }
+
+    public SingleInstanceGate(string name)
+    {
+        _mutex = new Mutex(true, name, out bool createdNew);
+        IsPrimaryInstance = createdNew;
+    }
+
+    public void Dispose()
+    {
+        try
+        {
+            if (IsPrimaryInstance)
+                _mutex.ReleaseMutex();
+        }
+        catch
+        {
+        }
+
+        _mutex.Dispose();
+    }
+}
