503f97c699552cdda9364613332a4c3d0aa97ca3
IndustrialCodeGuy <Dmichel@farmward.net>
2026-08-09 02:10:57 -0500

    Add files via upload

---
+++ Shared.Shell/Models/DriveSnapshot.cs
@@ -0,0 +1,27 @@
+﻿namespace Shared.Shell.Models;
+
+public sealed record DriveSnapshot
+{
+    public required string DriveRoot { get; init; }
+    public required string DisplayName { get; init; }
+    public string? VolumeLabel { get; init; }
+
+    public DriveType DriveType { get; init; }
+    public bool IsReady { get; init; }
+    public bool IsPresent { get; init; } = true;
+    public bool IsSystemDrive { get; init; }
+
+    public bool IsBitLockerProtected { get; init; }
+    public bool IsBitLockerLocked { get; init; }
+
+    public DriveIssueKind IssueKind { get; init; }
+    public int? IssueHResult { get; init; }
+    public string? IssueMessage { get; init; }
+
+    public bool IsEffectivelyBitLockerLocked => IsBitLockerLocked || IssueKind == DriveIssueKind.BitLockerLocked;
+
+    public long? TotalSizeBytes { get; init; }
+    public long? FreeSpaceBytes { get; init; }
+
+    public DriveVisualKind VisualKind { get; init; }
+}
---
+++ Shared.Shell/Models/DriveVisualKind.cs
@@ -0,0 +1,17 @@
+﻿namespace Shared.Shell.Models
+{
+    public enum DriveVisualKind
+    {
+        Fixed,
+        System,
+        Network,
+        Removable,
+        Optical,
+        BitLockerLocked,
+        BitLockerStatusUnknown,
+        BitLockerUnlocked,
+        BitLockerProtectionOff,
+        SystemBitLockerUnlocked,
+        SystemBitLockerProtectionOff
+    }
+}
