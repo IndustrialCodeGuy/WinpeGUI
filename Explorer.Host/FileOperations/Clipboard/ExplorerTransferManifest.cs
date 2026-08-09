0bb0b0959ce522978ab6101337b66280d835866b
IndustrialCodeGuy <Dmichel@farmward.net>
2026-08-09 01:57:57 -0500

    Add files via upload

---
+++ Explorer.Host/FileOperations/Clipboard/ExplorerTransferManifest.cs
@@ -0,0 +1,10 @@
+﻿namespace Explorer.Host.FileOperations.Clipboard;
+
+internal sealed class ExplorerTransferManifest
+{
+    public int Version { get; init; } = 1;
+
+    public List<string> SourcePaths { get; init; } = [];
+
+    public bool Move { get; init; }
+}
